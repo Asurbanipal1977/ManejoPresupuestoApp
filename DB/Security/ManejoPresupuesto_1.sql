fc9657bbfd5459df21b10f641900dd386f83133d
Asurbanipal1977 <Asurbanipal1977@gmail.com>
2022-02-02 12:50:15 +0100

    Cambios

--- DB/Security/ManejoPresupuesto_1.sql
+++ DB/Security/ManejoPresupuesto_1.sql
@@ -1,3 +1,3 @@
 ﻿CREATE LOGIN [ManejoPresupuesto]
-    WITH PASSWORD = N'a>qav{lg2CNqpXss_txv#84|msFT7_&#$!~<zosHnwgiz x{', SID = 0x3699E19F1CA76249BDDB0E825A43183F, DEFAULT_LANGUAGE = [Español], CHECK_POLICY = OFF;
+    WITH PASSWORD = N'a>qav{lg2CNqpXss_txv#84|msFT7_&#$!~<zosHnwgiz x{', SID = 0x3699E19F1CA76249BDDB0E825A43183F;
 
